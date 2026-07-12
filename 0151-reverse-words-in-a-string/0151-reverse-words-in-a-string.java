03bf16d25a1e44fe8684eb4aba1894d17cf58e49
Harshal Suryawanshi <149818377+Harshal24-01@users.noreply.github.com>
2026-07-12 22:47:15 +0530

    Time: 8 ms (36.25%), Space: 44.3 MB (69.91%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,21 +1,64 @@
 class Solution {
     public String reverseWords(String s) {
-        //__hello__world__
-        String trim = s.trim();
-        //hello__world
-        String[] arr = trim.split("\\s+");
-        // [hello,__,world]
+        int l = 0, r = s.length() - 1;
+        while (l < s.length()) {
+            if (s.charAt(l) == ' ') {
+                l++;
+            } else {
+                break;
+            }
+        }
+
+        while (r >= 0) {
+            if (s.charAt(r) == ' ') {
+                r--;
+            } else {
+                break;
+            }
+        }
+
+        StringBuilder sB = new StringBuilder();
+
+        while (l <= r) {
+            if (s.charAt(l) != ' ') {
+                sB.append(s.charAt(l));
+                l++;
+            } else if (s.charAt(l) == ' ') {
+                if (sB.charAt(sB.length() - 1) != ' ') {
+                    sB.append(' ');
+                    l++;
+                } else {
+                    l++;
+                }
+            }
+        }
         int i = 0;
-        int j = arr.length - 1;
+        int j = sB.length() - 1;
         while (i < j) {
-            String temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            char temp = sB.charAt(i);
+            sB.setCharAt(i, sB.charAt(j));
+            sB.setCharAt(j, temp);
             i++;
             j--;
         }
-        //[world,__,hello]
-        return String.join(" ", arr);
-        // world__hello
+
+        int start = 0;
+        int end = 0;
+        while (start < sB.length()) {
+            while (end < sB.length() && sB.charAt(end) != ' ') {
+                end++;
+            }
+            int p1 = start, p2 = end-1;
+            while (p1 < p2) {
+                char temp = sB.charAt(p1);
+                sB.setCharAt(p1, sB.charAt(p2));
+                sB.setCharAt(p2, temp);
+                p1++;
+                p2--;
+            }
+            start = end + 1;
+            end = start;
+        }
+        return sB.toString();
     }
 }
